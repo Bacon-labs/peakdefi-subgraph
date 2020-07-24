c91cba9b2fddffbdd3e6161a12a8a6f463f858ca
Zefram Lou <zeframlou@gmail.com>
2020-07-23 23:18:36 -0700

    Started PeakStaking and PeakReward mappings

---
+++ src/mappings/peakReward.ts
@@ -0,0 +1,27 @@
+import {
+  Register as RegisterEvent,
+  RankChange as RankChangeEvent,
+  PayCommission as PayCommissionEvent,
+  ChangedCareerValue as ChangedCareerValueEvent
+} from '../../generated/PeakStaking/PeakStaking'
+import {
+  PeakUser,
+  PeakActivity
+} from '../../generated/schema'
+import * as Utils from '../utils'
+
+export function handleRegister(event: RegisterEvent): void {
+
+}
+
+export function handleRankChange(event: RankChangeEvent): void {
+    
+}
+
+export function handlePayCommission(event: PayCommissionEvent): void {
+    
+}
+
+export function handleChangedCareerValue(event: ChangedCareerValueEvent): void {
+    
+}
---
+++ src/mappings/peakStaking.ts
@@ -0,0 +1,24 @@
+import {
+  CreateStake as CreateStakeEvent,
+  WithdrawReward as WithdrawRewardEvent,
+  WithdrawStake as WithdrawStakeEvent
+} from '../../generated/PeakStaking/PeakStaking'
+import {
+  PeakStakingPool,
+  PeakUser,
+  PeakStakeEntry,
+  PeakActivity
+} from '../../generated/schema'
+import * as Utils from '../utils'
+
+export function handleCreateStake(event: CreateStakeEvent): void {
+
+}
+
+export function handleWithdrawReward(event: WithdrawRewardEvent): void {
+    
+}
+
+export function handleWithdrawStake(event: WithdrawStakeEvent): void {
+    
+}
